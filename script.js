b7aa999781468c665f70e1c0625f2decc2a0fff3
470M <165724285+EthanDupe@users.noreply.github.com>
2024-05-06 22:19:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ c.height = window.innerHeight;
 c.width = window.innerWidth;
 
 //chinese characters - taken from the unicode charset
-var chinese = "田由甲申甴电甶男甸甹町画甼甽甾甿畀畁畂畃畄畅畆畇畈畉畊畋界畍畎畏畐畑";
+var chinese = "ABCDEFGHIGCLMNOPQRSTUVWXYZ";
 //converting the string into an array of single characters
 chinese = chinese.split("");
 
