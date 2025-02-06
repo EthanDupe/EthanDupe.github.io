2677dfd0b269238948390ce04d0c23fc431f0d81
IronicallyUnironic <randomgithub@outlook.com>
2025-02-06 17:54:30 +0000

    updating

--- jarbeefis/james/firewater/res/js/main.js
+++
@@ -1,8 +0,0 @@
-import { playGame } from "./game.js";
-import { loadData, loadDataFromLocalStorage } from "./helpers.js";
-
-window.onload = async () => {
-    await loadData();
-    loadDataFromLocalStorage();
-    playGame();
-};
